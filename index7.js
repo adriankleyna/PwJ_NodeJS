947f21a0778ae7a64d45c4328dd53086069513ea
adriankleyna <61495980+adriankleyna@users.noreply.github.com>
2020-12-09 12:16:12 +0100

    gotowe
    Lab10

---
+++ index7.js
@@ -0,0 +1,71 @@
+// const http = require('http');
+// const fs = require('fs');
+// http.createServer(function(req, res) {
+//     if (req.url == '/') {
+//         fs.readFile('./titles.json', function(err, data) {
+//             if (err) {
+//                 console.error(err);
+//                 res.end('titles.json error');
+//             }
+//             else {
+//                 let titles = JSON.parse(data.toString());
+//                 fs.readFile('./template.html', function(err, data) {
+//                     if (err) {
+//                         console.error(err);
+//                         res.end('template.html error');
+//                     }
+//                     else {
+//                         let tmpl = data.toString();
+//                         let html = tmpl.replace('%', titles.join('</li><li>'));
+//                         res.writeHead(200, {'Content-Type': 'text/html'});
+//                         res.end(html);
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// }).listen(8000, "127.0.0.1");
+
+//2.
+
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function (req, res) {
+    getTitles(res);
+}).listen(8000, "127.0.0.1");
+
+// function getTitles(res) {
+//     fs.readFile('./titles.json', function (err, data) {
+//         if (err) {
+//             hadError(err, res);
+//         } else {
+//             getTemplate(JSON.parse(data.toString()), res);
+//         }
+//     });
+// }
+
+function getTitles(res) {
+    fs.readFile('./titles.json', function (err, data) {
+        if (err) return hadError(err, res);
+        getTemplate(JSON.parse(data.toString()), res);
+    });
+};
+
+function getTemplate(titles, res) {
+    fs.readFile('./template.html', function (err, data) {
+        if (err) return hadError(err, res);
+        formatHtml(titles, data.toString(), res);
+
+    });
+}
+
+function formatHtml(titles, tmpl, res) {
+    let html = tmpl.replace('%', titles.join('</li><li>'));
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+}
+
+function hadError(err, res) {
+    console.error(err);
+    res.end('Error');
+}
